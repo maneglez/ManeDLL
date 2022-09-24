3e4b686e9763f9bff9b49626f92bf39271433ec9
Mane Glez <maneg9394@gmail.com>
2022-09-24 17:07:27 -0500

    smn

--- Mane.BD/Bd/Bd.SQLite.cs
+++ Mane.BD/Bd/Bd.SQLite.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ;
+
 
 namespace Mane.BD
 {
-    public static partial class Bd
-    {
-        public DataTable executeQuerySQLite(string query,string connName)
-        {
-            using (SQLiteConnection db = new SQLiteConnection(_path))
-            {
-                db.CreateTable<Configuracion>();
-            }
-        }
-    }
+    //public static partial class Bd
+    //{
+    //    public DataTable executeQuerySQLite(string query,string connName)
+    //    {
+    //        using (SQLiteConnection db = new SQLiteConnection(_path))
+    //        {
+    //            db.CreateTable<Configuracion>();
+    //        }
+    //    }
+    //}
 }
