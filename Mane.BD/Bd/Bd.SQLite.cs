b47ab701afa24d3aebd580b6aa15f2d78a46f1d0
SoporteTie <soporte@tie.com.mx>
2022-09-30 18:32:33 -0500

    se comentó lo de sqlite

--- Mane.BD/Bd/Bd.SQLite.cs
+++ Mane.BD/Bd/Bd.SQLite.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ;
+
 
 namespace Mane.BD
 {
     public static partial class Bd
     {
-        public DataTable executeQuerySQLite(string query,string connName)
-        {
-            using (SQLiteConnection db = new SQLiteConnection(_path))
-            {
-                db.CreateTable<Configuracion>();
-            }
-        }
+        //public DataTable executeQuerySQLite(string query,string connName)
+        //{
+        //    using (SQLiteConnection db = new SQLiteConnection(_path))
+        //    {
+        //        db.CreateTable<Configuracion>();
+        //    }
+        //}
     }
 }
