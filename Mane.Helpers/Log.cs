3addab8bf99be655bfbb60dce5efbc6adadbe8e7
Emmanuel Gonzalez <egonzalez@tie.com.mx>
2023-02-14 19:20:26 -0600

    log de objetos

--- Mane.Helpers/Log.cs
+++ Mane.Helpers/Log.cs
@@ -20,20 +20,24 @@ namespace Mane.Helpers
         /// <summary>
         /// Genera o agrega una lína al archivo de log
         /// </summary>
-        /// <param name="sLog">Cadena a registrar</param>
+        /// <param name="texto">Cadena a registrar</param>
         /// <param name="dir">Carpeta donde se guardará el archivo por defecto se tomará el establecido en la propiedad:
         /// "RutaLogPorDefecto"</param>
-        public static void Add(string sLog, string dir = "")
+        public static void Add(string texto, string dir = "")
         {
             var hoy = DateTime.Now;
             var nombreArchivo = "log_" + hoy.Year + "_" + hoy.Month + "_" + hoy.Day + ".txt";
-            sLog = hoy + " - " + sLog + Environment.NewLine;
+            texto = hoy + " - " + texto + Environment.NewLine;
             var ruta = "";
             if (string.IsNullOrEmpty(dir))
                 ruta = Path.Combine(RutaLogPorDefecto, nombreArchivo);
             else
                 ruta = Path.Combine(dir, nombreArchivo);
-            Archivos.EscribirEnArchivo(sLog, ruta);
+            Archivos.EscribirEnArchivo(texto, ruta);
+        }
+        public static void Add(object obj,string dir = "")
+        {
+            Add(obj?.ToString(), dir);
         }
     }
 }
