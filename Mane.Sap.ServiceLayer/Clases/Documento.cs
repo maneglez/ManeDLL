b1289774de2592fecb6104ec838bfcb4df4e2ae9
Emmanuel Gonzalez <egonzalez@tie.com.mx>
2023-07-25 08:35:16 -0600

    Se actualizaron varias cosillas

--- Mane.Sap.ServiceLayer/Clases/Documento.cs
+++ Mane.Sap.ServiceLayer/Clases/Documento.cs
@@ -9,7 +9,16 @@ using Newtonsoft.Json.Linq;
 
 namespace Mane.Sap.ServiceLayer.Clases
 {
-   
+   public class Documento
+    {
+        [JsonProperty(PropertyName = CamposDeUsuario.Docs_UUID)]
+        public string UUID { get; set; }
+    }
+
+    public static class CamposDeUsuario
+    {
+        public const string Docs_UUID = "U_UUID";
+    }
 
 
 
