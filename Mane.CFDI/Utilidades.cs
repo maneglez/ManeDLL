ea3e1af5ed22f5236e1ecdd791ed1be3c4959f87
Emmanuel Gonzalez <egonzalez@tie.com.mx>
2023-08-03 12:00:44 -0600

    cosillas

---
+++ Mane.CFDI/Utilidades.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mane.CFDI
+{
+    public static class Utilidades
+    {
+        //Core recursion function
+        private static XElement RemoveAllNamespaces(XElement xmlDocument)
+        {
+            if (!xmlDocument.HasElements)
+            {
+                XElement xElement = new XElement(xmlDocument.Name.LocalName);
+                xElement.Value = xmlDocument.Value;
+
+                foreach (XAttribute attribute in xmlDocument.Attributes())
+                    if (string.IsNullOrEmpty(attribute.Name.NamespaceName))
+                        xElement.Add(attribute);
+
+                return xElement;
+            }
+            else
+            {
+                var ele = new XElement(xmlDocument.Name.LocalName, xmlDocument.Elements().Select(el => RemoveAllNamespaces(el)));
+
+                foreach (XAttribute attribute in xmlDocument.Attributes())
+                    if (string.IsNullOrEmpty(attribute.Name.NamespaceName))
+                        ele.Add(attribute);
+                return ele;
+            }
+        }
+    }
+}
