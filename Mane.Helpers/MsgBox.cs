9d13ff7276557813011cb75b5f14184519a42400
Emmanuel Gonzalez <egonzalez@tie.com.mx>
2023-08-28 18:47:47 -0600

    modulo de picking funcionando

--- Mane.Helpers/MsgBox.cs
+++ Mane.Helpers/MsgBox.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace Mane.Helpers
 {
     public static class MsgBox
     {
+        public static bool DebugMode;
+        public static void debug(Exception ex) => debug(ex.ToString());
+        public static void debug(string msg) { if (DebugMode) MessageBox.Show(msg, "DebugMsg"); }
         public static void normal(string msg, string titulo = "") => MessageBox.Show(msg, titulo);
         public static void warning(string msg, string titulo = "Advertencia") => MessageBox.Show(msg, titulo, MessageBoxButtons.OK, MessageBoxIcon.Warning);
         public static void info(string msg, string titulo = "Info") => MessageBox.Show(msg, titulo, MessageBoxButtons.OK, MessageBoxIcon.Information);
