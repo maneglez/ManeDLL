256cffcf76aa42300831c2fe8b9686b61545fddb
Mane Glez <maneg9394@gmail.com>
2023-11-13 15:56:40 -0600

    Se agrego encriptado de contraseñas a la libreria de SAP, y la modificacion de la conexion tambien esta protegida con contraseña

---
+++ Mane.Sap/Forms/ConfirmarContrasena.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace Mane.Sap.Forms
+{
+    public partial class ConfirmarContrasena : Form
+    {
+        private static int Intentos = 0;
+        private static int MaximoIntentos = 5;
+        private string[] Contrasenas = new string[]
+        {
+            "maneg13",
+            "tie.2023",
+            "saoilkew",
+            "saoilkew12",
+            "mane1234",
+            "sapo12"
+        };
+        private string password { get => textBox1.Text; set => textBox1.Text = value; }
+        private string rutaArchivoConfig;
+        public ConfirmarContrasena(string RutaArchivoConfig = "")
+        {
+            InitializeComponent();
+            rutaArchivoConfig = RutaArchivoConfig;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ValidarPass();
+            }
+        }
+
+        private void ValidarPass()
+        {
+            if (Intentos >= MaximoIntentos)
+            {
+                MessageBox.Show("Se excedió el numero máximo de intentos");
+                Close();
+                Dispose();
+                return;
+            }
+            if (!Contrasenas.Contains(password))
+            {
+                Intentos++;
+                MessageBox.Show("Contraseña incorrecta");
+                return;
+            }
+            Intentos = 0;
+            var fm = new ConnectionManager(rutaArchivoConfig);
+            fm.Show();
+            Close();
+            Dispose();
+        }
+    }
+}
