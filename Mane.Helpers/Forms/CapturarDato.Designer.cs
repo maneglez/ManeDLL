b8d2f0a0dca21e32fca6633828832c18cc6b61a5
Emmanuel Gonzalez <egonzalez@tie.com.mx>
2023-12-11 10:04:57 -0600

    Se agregó formulario para capturar texto

---
+++ Mane.Helpers/Forms/CapturarDato.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Mane.Helpers.Forms
+{
+    partial class CapturarDato
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTextoPeticion = new System.Windows.Forms.Label();
+            this.tbDato = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTextoPeticion
+            // 
+            this.lbTextoPeticion.AutoSize = true;
+            this.lbTextoPeticion.Location = new System.Drawing.Point(12, 18);
+            this.lbTextoPeticion.Name = "lbTextoPeticion";
+            this.lbTextoPeticion.Size = new System.Drawing.Size(71, 13);
+            this.lbTextoPeticion.TabIndex = 4;
+            this.lbTextoPeticion.Text = "Ingresar Dato";
+            // 
+            // tbDato
+            // 
+            this.tbDato.Location = new System.Drawing.Point(12, 34);
+            this.tbDato.Name = "tbDato";
+            this.tbDato.Size = new System.Drawing.Size(255, 20);
+            this.tbDato.TabIndex = 0;
+            this.tbDato.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbDato_KeyDown);
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(15, 61);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(96, 61);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // CapturarDato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 93);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.tbDato);
+            this.Controls.Add(this.lbTextoPeticion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "CapturarDato";
+            this.Text = "Ingresar Dato";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTextoPeticion;
+        private System.Windows.Forms.TextBox tbDato;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Mane.Helpers/Forms/CapturarDato.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mane.Helpers.Forms
+{
+    public partial class CapturarDato : Form
+    {
+        public string Titulo { get => Text; set => Text = value; }
+        public string Dato { get => tbDato.Text; set => tbDato.Text = value; }
+        public string TextoPeticion { get => lbTextoPeticion.Text; set => lbTextoPeticion.Text = value; }
+        public CapturarDato()
+        {
+            InitializeComponent();
+        }
+
+        public CapturarDato( string textoPeticion, string titulo = null,string dato = null)
+        {
+            InitializeComponent();
+            if (!string.IsNullOrWhiteSpace(titulo))
+                Titulo = titulo;
+            if(!string.IsNullOrWhiteSpace(dato))
+                Dato = dato;
+            TextoPeticion = textoPeticion;
+        }
+
+        private void tbDato_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+    }
+}
